1f66cfe721ddcc4181581ec4155e9012687f09b7
SEMINE2002 <seminesem2@gmail.com>
2026-06-26 21:41:00 +0200

    mise à jour

--- sbee-sport-api/config/database.php
+++ sbee-sport-api/config/database.php
@@ -44,12 +44,12 @@ return [
             'transaction_mode' => 'DEFERRED',
         ],
 
-    'mysql' => [
+   'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
+            'database' => env('DB_DATABASE', 'sbee_sport'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
@@ -58,13 +58,10 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => 'InnoDB ROW_FORMAT=DYNAMIC',
-            'options' => [
-                      1002 => 2, // PDO::MYSQL_ATTR_SSL_MODE => 2 (REQUIRED)
-                      1011 => 0, // PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => 0 (false)
-                      // 1016 est la valeur pour PDO::MYSQL_ATTR_SSL_CA
-                      1016 => base_path('certs/ca.pem'), // Chemin vers votre fichier
-                    ],
+            'engine' => 'InnoDB',
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'mariadb' => [
---
+++ sbee-sport-api/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $this->call([
+            // Ajoutez ici tous vos seeders dans l'ordre souhaité
+            UserSeeder::class,
+            RoleSeeder::class,
+            // TypeMaterielSeeder::class,
+        ]);
+    }
+}
